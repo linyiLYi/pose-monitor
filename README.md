59ac422560ddfdc736f31d5bd96fed16e7061540
linyiLYi <48440925+linyiLYi@users.noreply.github.com>
2022-10-29 08:16:31 +0800

    update android package and readme

--- README.md
+++ README.md
@@ -1,74 +1,46 @@
-“让爷康康”是一款手机 AI 应用程序，可以监测不良坐姿并进行语音提示
-# TensorFlow Lite Pose Estimation Android Demo
+# PoseMon 让爷康康
 
-### Overview
-This is an app that continuously detects the body parts in the frames seen by
-your device's camera. These instructions walk you through building and running
-the demo on an Android device. Camera captures are discarded immediately after
-use, nothing is stored or saved.
+## 介绍
 
-The app demonstrates how to use 4 models:
+<image align="right" src="doc_images/screenshot_icon.jpg" alt="Application Icon" width=17%>
 
-* Single pose models: The model can estimate the pose of only one person in the
-input image. If the input image contains multiple persons, the detection result
-can be largely incorrect.
-   * PoseNet
-   * MoveNet Lightning
-   * MoveNet Thunder
-* Multi pose models: The model can estimate pose of multiple persons in the
-input image.
-   * MoveNet MultiPose: Support up to 6 persons.
+“让爷康康”是一款应用于安卓平台的手机应用，可以实时监测不良坐姿并给出语音提示。本项目主要基于 [Tensorflow Lite 官方示例 - 姿态估计](https://github.com/tensorflow/examples/tree/master/lite/examples/pose_estimation/android)实现，其中 AI 部分包含用于姿态估计的 [MoveNet](https://blog.tensorflow.org/2021/05/next-generation-pose-detection-with-movenet-and-tensorflowjs.html)，以及用于对姿态进行分类的[全连接网络](https://github.com/tensorflow/tensorflow/blob/master/tensorflow/lite/g3doc/tutorials/pose_classification.ipynb)。本应用不需要联网使用，所有 AI 特性均在手机本地运行，不需要将视频画面传输至外部服务器，仅需要摄像头权限用于获取姿态画面。视频介绍可以点击 [bilibili]() 或 [YouTube]()。
 
-See this [blog post](https://blog.tensorflow.org/2021/05/next-generation-pose-detection-with-movenet-and-tensorflowjs.html)
-for a comparison between these models.
+## 在 Android Studio 中编译程序并运行
 
-![Demo Image](posenetimage.png)
+### 准备工作
 
-## Build the demo using Android Studio
+* 安卓项目的编译需要 Android Studio，可以进入[官方网站](
+ https://developer.android.com/studio/install?hl=zh-cn)按照说明进行下载安装。
 
-### Prerequisites
+* 需要准备一部安卓手机。
 
-* If you don't have it already, install **[Android Studio](
- https://developer.android.com/studio/index.html)** 4.2 or
- above, following the instructions on the website.
+### 编译程序
 
-* Android device and Android development environment with minimum API 21.
+* 通过 `git clone` 克隆本项目，或者以压缩包形式下载项目文件并解压。
 
-### Building
-* Open Android Studio, and from the `Welcome` screen, select
-`Open an existing Android Studio project`.
+* 打开 Android Studio，在初始的 `Welcome` 界面选择
+`Open an existing Android Studio project`，打开项目中的安卓工程文件夹。
 
-* From the `Open File or Project` window that appears, navigate to and select
- the `lite/examples/pose_estimation/android` directory from wherever you
- cloned the `tensorflow/examples` GitHub repo. Click `OK`.
+* 安卓工程文件位于本项目的 `android/` 文件夹下。在 Android Studio 的提示窗口中选择该文件夹。项目打开后软件可能会提示需要进行 Gradle 同步，同意并等待同步完成即可。
 
-* If it asks you to do a `Gradle Sync`, click `OK`.
+* 将处于开发者模式的手机通过 USB 线连接到电脑，具体连接方法可以参考[官方教程](https://developer.android.com/studio/run/device?hl=zh-cn)。如果程序顶部工具栏右侧正确显示了你的手机型号，说明设备连接成功。
 
-* You may also need to install various platforms and tools, if you get errors
- like `Failed to find target with hash string 'android-21'` and similar. Click
- the `Run` button (the green arrow) or select `Run` > `Run 'android'` from the
- top menu. You may need to rebuild the project using `Build` > `Rebuild Project`.
+* 如果是首次安装 Android Studio，可能还需要安装一系列开发工具。点击软件界面右上角的绿色三角按钮`Run 'app'`直接运行程序。如果有需要安装的工具，系统会进行提示，按照提示依次安装即可。
 
-* If it asks you to use `Instant Run`, click `Proceed Without Instant Run`.
+### 模型介绍
 
-* Also, you need to have an Android device plugged in with developer options
- enabled at this point. See **[here](
- https://developer.android.com/studio/run/device)** for more details
- on setting up developer devices.
+本项目需要用到两个神经网络模型文件，均已包含在本项目中，不需要额外下载。第一个是 `int8` 格式的 MoveNet Thunder 神经网络模型，可以点击[官方模型文件链接](https://tfhub.dev/google/lite-model/movenet/singlepose/thunder/tflite/int8/4)进一步了解。[MoveNet](https://blog.tensorflow.org/2021/05/next-generation-pose-detection-with-movenet-and-tensorflowjs.html) 是谷歌推出的轻量级人体姿态估计模型，有 Thunder 和 Lightning 两个版本。其中 Thunder 版本运行速度较慢，但准确率更高，本项目使用的是 Thunder 版本。该版本又分为 `float16`、`int8` 两种数据格式。其中 `float16` 模型只能在通用 GPU 上运行，而 `int8` 模型既可以运行于通用 GPU 之上，也可以在高通骁龙处理器的 [Hexagon DSP 数字信号处理器](https://developer.qualcomm.com/software/hexagon-dsp-sdk/dsp-processor)上运行。运行在 Hexagon 处理器上时，AI 程序运行速度更快、也更省电，建议对 AI 模型进行移动部署时优先选择 Hexagon 处理器。目前谷歌也推出了自研的 Google Tensor 处理器，最新型号为 Tensor G2，如何调用 Tensor 处理器的 AI 加速单元尚不清楚，未来拿到设备实测确认后会更新文档。
+
+#### 训练自己的分类网络
+
+<image align="right" src="doc_images/labeled_movenet_result.png" alt="17 Keypoints detected by MoveNet" width=17%>
+
+除了 MoveNet Thunder，本项目还使用了一个简单的全连接网络对 MoveNet 输出的姿态信息（人体 17 个关键点的坐标）进行分类，用来判断画面中的人处于“标准坐姿”、“翘二郎腿”、“脖子前倾驼背”中的哪一种状态。关于该分类网络的介绍以及训练过程实际演示，可以参考 Tensorflow Lite 的 [Jupyter Notebook 教程](https://github.com/tensorflow/tensorflow/blob/master/tensorflow/lite/g3doc/tutorials/pose_classification.ipynb)，或是本项目中修改并注释过的[版本]()。本项目为了对“标准坐姿”、“翘二郎腿”、“脖子前倾驼背”三种姿态进行分类，为每种姿态采集了约 300 张照片作为训练集（共 876 张照片），为每种姿态采集了约 30 张作为测试集（共 74 张照片）。其中训练集与测试集为不同人物主体，以此来在训练过程中及时发现模型的过拟合问题。训练数据应存放于 `main/pose_data/train/` 路径下的 `standard`、`crossleg`、`forwardhead` 
+三个文件夹中，测试数据则位于 `main/pose_data/test/` 路径下。本项目中用于训练分类网络的 [Jupyter Notebook]() 会将原始数据自动转化为训练数据包，在此过程中生成每张照片的 MoveNet 检测结果，并将每张照片标记为三种姿态中的一种，最后将所有信息存储在 `main/pose_data/train_data.csv`、`main/pose_data/test_data.csv`，并生成记录标签信息的文本文件 `main/pose_data/pose_labels.txt`。在 Notebook 中训练完毕后，在 `main/pose_data/` 路径下会自动生成 `.tflite` 权重文件，导入至 Android Studio 项目中，替换掉本项目中的 `android\app\src\main\assets\classifier.tflite` 即可使用。
+
+### 运行效果
 
 
-### Model used
-Downloading, extraction and placement in assets folder has been managed
- automatically by `download.gradle`.
-
-If you explicitly want to download the model, you can download it from here:
-
-* [Posenet](https://storage.googleapis.com/download.tensorflow.org/models/tflite/posenet_mobilenet_v1_100_257x257_multi_kpt_stripped.tflite)
-* [Movenet Lightning](https://tfhub.dev/google/movenet/singlepose/lightning/)
-* [Movenet Thunder](https://tfhub.dev/google/movenet/singlepose/thunder/)
-* [Movenet MultiPose](https://tfhub.dev/google/movenet/multipose/lightning/)
-
-### Additional Note
-_Please do not delete the assets folder content_. If you explicitly deleted the
- files, then please choose `Build` > `Rebuild` from menu to re-download the
- deleted model files into assets folder.
+## 鸣谢
+本项目主要基于 [Tensorflow Lite Pose Estimation 示例项目](https://github.com/tensorflow/examples/tree/master/lite/examples/pose_estimation/android)，离不开 [Tensorflow](https://www.tensorflow.org/?hl=zh-cn)、[Jupyter Notebook](https://jupyter.org/) 等开源框架、开源开发工具。感谢各位程序工作者对开源社区的贡献！
---
+++ android/app/src/main/java/lyi/linyi/posemon/MainActivity.kt
@@ -0,0 +1,360 @@
+/* Copyright 2022 Lin Yi. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================
+*/
+
+/** 本应用主要对 Tensorflow Lite Pose Estimation 示例项目的 MainActivity.kt
+ *  文件进行了重写，示例项目中其余文件除了包名调整外基本无改动，原版权归
+ *  The Tensorflow Authors 所有 */
+
+package lyi.linyi.posemon
+
+import android.Manifest
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.pm.PackageManager
+import android.media.MediaPlayer
+import android.os.Bundle
+import android.os.Process
+import android.view.SurfaceView
+import android.view.View
+import android.view.WindowManager
+import android.widget.*
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.DialogFragment
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import lyi.linyi.posemon.camera.CameraSource
+import lyi.linyi.posemon.data.Device
+import lyi.linyi.posemon.ml.ModelType
+import lyi.linyi.posemon.ml.MoveNet
+import lyi.linyi.posemon.ml.PoseClassifier
+
+class MainActivity : AppCompatActivity() {
+    companion object {
+        private const val FRAGMENT_DIALOG = "dialog"
+    }
+
+    /** 为视频画面创建一个 SurfaceView */
+    private lateinit var surfaceView: SurfaceView
+
+    /** 修改默认计算设备：CPU、GPU、NNAPI（AI加速器） */
+    private var device = Device.NNAPI
+
+    /** 定义几个计数器 */
+    private var forwardheadCounter = 0
+    private var crosslegCounter = 0
+    private var standardCounter = 0
+    private var missingCounter = 0
+
+    /** 定义一个历史姿态寄存器 */
+    private var poseRegister = "standard"
+
+    /** 设置一个用来显示 Debug 信息的 TextView */
+    private lateinit var tvDebug: TextView
+
+    /** 设置一个用来显示当前坐姿状态的 ImageView */
+    private lateinit var ivStatus: ImageView
+
+    private lateinit var tvFPS: TextView
+    private lateinit var tvScore: TextView
+    private lateinit var spnDevice: Spinner
+
+    private var cameraSource: CameraSource? = null
+    private var isClassifyPose = true
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                /** 得到用户相机授权后，程序开始运行 */
+                openCamera()
+            } else {
+                /** 提示用户“未获得相机权限，应用无法运行” */
+                ErrorDialog.newInstance(getString(R.string.tfe_pe_request_permission))
+                    .show(supportFragmentManager, FRAGMENT_DIALOG)
+            }
+        }
+
+    private var changeDeviceListener = object : AdapterView.OnItemSelectedListener {
+        override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+            changeDevice(position)
+        }
+
+        override fun onNothingSelected(parent: AdapterView<*>?) {
+            /** 如果用户未选择运算设备，使用默认设备进行计算 */
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        /** 程序运行时保持屏幕常亮 */
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+        tvScore = findViewById(R.id.tvScore)
+
+        /** 用来显示 Debug 信息 */
+        tvDebug = findViewById(R.id.tvDebug)
+
+        /** 用来显示当前坐姿状态 */
+        ivStatus = findViewById(R.id.ivStatus)
+
+        tvFPS = findViewById(R.id.tvFps)
+        spnDevice = findViewById(R.id.spnDevice)
+        surfaceView = findViewById(R.id.surfaceView)
+        initSpinner()
+        if (!isCameraPermissionGranted()) {
+            requestPermission()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        openCamera()
+    }
+
+    override fun onResume() {
+        cameraSource?.resume()
+        super.onResume()
+    }
+
+    override fun onPause() {
+        cameraSource?.close()
+        cameraSource = null
+        super.onPause()
+    }
+
+    /** 检查相机权限是否有授权 */
+    private fun isCameraPermissionGranted(): Boolean {
+        return checkPermission(
+            Manifest.permission.CAMERA,
+            Process.myPid(),
+            Process.myUid()
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun openCamera() {
+        /** 音频播放 */
+        val crosslegPlayer = MediaPlayer.create(this, R.raw.crossleg_1)
+        val forwardheadPlayer = MediaPlayer.create(this, R.raw.forwardhead_1)
+        val standardPlayer = MediaPlayer.create(this, R.raw.standard_2)
+        var crosslegPlayerFlag = true
+        var forwardheadPlayerFlag = true
+        var standardPlayerFlag = true
+
+        if (isCameraPermissionGranted()) {
+            if (cameraSource == null) {
+                cameraSource =
+                    CameraSource(surfaceView, object : CameraSource.CameraSourceListener {
+                        override fun onFPSListener(fps: Int) {
+
+                            /** 解释一下，tfe_pe_tv 的意思：tensorflow example、pose estimation、text view */
+                            tvFPS.text = getString(R.string.tfe_pe_tv_fps, fps)
+                        }
+
+                        /** 对检测结果进行处理 */
+                        override fun onDetectedInfo(
+                            personScore: Float?,
+                            poseLabels: List<Pair<String, Float>>?
+                        ) {
+                            tvScore.text = getString(R.string.tfe_pe_tv_score, personScore ?: 0f)
+
+                            /** 分析目标姿态，给出提示 */
+                            if (poseLabels != null && personScore != null && personScore > 0.3) {
+                                missingCounter = 0
+                                val sortedLabels = poseLabels.sortedByDescending { it.second }
+                                when (sortedLabels[0].first) {
+                                    "forwardhead" -> {
+                                        crosslegCounter = 0
+                                        standardCounter = 0
+                                        if (poseRegister == "forwardhead") {
+                                            forwardheadCounter++
+                                        }
+                                        poseRegister = "forwardhead"
+
+                                        /** 显示当前坐姿状态：脖子前伸 */
+                                        if (forwardheadCounter > 60) {
+
+                                            /** 播放提示音 */
+                                            if (forwardheadPlayerFlag) {
+                                                forwardheadPlayer.start()
+                                            }
+                                            standardPlayerFlag = true
+                                            crosslegPlayerFlag = true
+                                            forwardheadPlayerFlag = false
+
+                                            ivStatus.setImageResource(R.drawable.forwardhead_confirm)
+                                        } else if (forwardheadCounter > 30) {
+                                            ivStatus.setImageResource(R.drawable.forwardhead_suspect)
+                                        }
+
+                                        /** 显示 Debug 信息 */
+                                        tvDebug.text = getString(R.string.tfe_pe_tv_debug, "${sortedLabels[0].first} $forwardheadCounter")
+                                    }
+                                    "crossleg" -> {
+                                        forwardheadCounter = 0
+                                        standardCounter = 0
+                                        if (poseRegister == "crossleg") {
+                                            crosslegCounter++
+                                        }
+                                        poseRegister = "crossleg"
+
+                                        /** 显示当前坐姿状态：翘二郎腿 */
+                                        if (crosslegCounter > 60) {
+
+                                            /** 播放提示音 */
+                                            if (crosslegPlayerFlag) {
+                                                crosslegPlayer.start()
+                                            }
+                                            standardPlayerFlag = true
+                                            crosslegPlayerFlag = false
+                                            forwardheadPlayerFlag = true
+                                            ivStatus.setImageResource(R.drawable.crossleg_confirm)
+                                        } else if (crosslegCounter > 30) {
+                                            ivStatus.setImageResource(R.drawable.crossleg_suspect)
+                                        }
+
+                                        /** 显示 Debug 信息 */
+                                        tvDebug.text = getString(R.string.tfe_pe_tv_debug, "${sortedLabels[0].first} $crosslegCounter")
+                                    }
+                                    else -> {
+                                        forwardheadCounter = 0
+                                        crosslegCounter = 0
+                                        if (poseRegister == "standard") {
+                                            standardCounter++
+                                        }
+                                        poseRegister = "standard"
+
+                                        /** 显示当前坐姿状态：标准 */
+                                        if (standardCounter > 30) {
+
+                                            /** 播放提示音：坐姿标准 */
+                                            if (standardPlayerFlag) {
+                                                standardPlayer.start()
+                                            }
+                                            standardPlayerFlag = false
+                                            crosslegPlayerFlag = true
+                                            forwardheadPlayerFlag = true
+
+                                            ivStatus.setImageResource(R.drawable.standard)
+                                        }
+
+                                        /** 显示 Debug 信息 */
+                                        tvDebug.text = getString(R.string.tfe_pe_tv_debug, "${sortedLabels[0].first} $standardCounter")
+                                    }
+                                }
+
+
+                            }
+                            else {
+                                missingCounter++
+                                if (missingCounter > 30) {
+                                    ivStatus.setImageResource(R.drawable.no_target)
+                                }
+
+                                /** 显示 Debug 信息 */
+                                tvDebug.text = getString(R.string.tfe_pe_tv_debug, "missing $missingCounter")
+                            }
+                        }
+                    }).apply {
+                        prepareCamera()
+                    }
+                isPoseClassifier()
+                lifecycleScope.launch(Dispatchers.Main) {
+                    cameraSource?.initCamera()
+                }
+            }
+            createPoseEstimator()
+        }
+    }
+
+    private fun isPoseClassifier() {
+        cameraSource?.setClassifier(if (isClassifyPose) PoseClassifier.create(this) else null)
+    }
+
+    /** 初始化运算设备选项菜单（CPU、GPU、NNAPI） */
+    private fun initSpinner() {
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.tfe_pe_device_name, android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+            spnDevice.adapter = adapter
+            spnDevice.onItemSelectedListener = changeDeviceListener
+        }
+    }
+
+    /** 在程序运行过程中切换运算设备 */
+    private fun changeDevice(position: Int) {
+        val targetDevice = when (position) {
+            0 -> Device.NNAPI
+            1 -> Device.GPU
+            else -> Device.CPU
+        }
+        if (device == targetDevice) return
+        device = targetDevice
+        createPoseEstimator()
+    }
+
+    private fun createPoseEstimator() {
+        val poseDetector = MoveNet.create(this, device, ModelType.Thunder)
+        poseDetector.let { detector ->
+            cameraSource?.setDetector(detector)
+        }
+    }
+
+    private fun requestPermission() {
+        when (PackageManager.PERMISSION_GRANTED) {
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
+            ) -> {
+                openCamera()
+            }
+            else -> {
+                requestPermissionLauncher.launch(
+                    Manifest.permission.CAMERA
+                )
+            }
+        }
+    }
+
+    /** 显示报错信息 */
+    class ErrorDialog : DialogFragment() {
+        override fun onCreateDialog(savedInstanceState: Bundle?): Dialog =
+            AlertDialog.Builder(activity)
+                .setMessage(requireArguments().getString(ARG_MESSAGE))
+                .setPositiveButton(android.R.string.ok) { _, _ ->
+                    // pass
+                }
+                .create()
+
+        companion object {
+
+            @JvmStatic
+            private val ARG_MESSAGE = "message"
+
+            @JvmStatic
+            fun newInstance(message: String): ErrorDialog = ErrorDialog().apply {
+                arguments = Bundle().apply { putString(ARG_MESSAGE, message) }
+            }
+        }
+    }
+}
